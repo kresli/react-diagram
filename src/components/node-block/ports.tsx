707f604a78af2693bf8605d5be6d00083325bdfd
Eduard Jacko <jacko.eduard@gmail.com>
2020-06-12 17:49:20 +0100

    center the port

--- src/components/node-block/ports.tsx
+++ src/components/node-block/ports.tsx
@@ -24,29 +24,25 @@ const Port: React.FunctionComponent<{
   port: IPortStore;
   onPortClick(): void;
 }> = ({ port, onPortClick }) => {
+  const gatePointStyle = {
+    background: "rgba(0,0,255, 1)",
+    width: 1,
+    height: 1,
+  };
   const gateRef = useRef<HTMLDivElement>(null);
+  const gate = (
+    <div className="gate" onClick={onPortClick}>
+      <div style={gatePointStyle} ref={gateRef} id={port.gateId} />
+    </div>
+  );
   useEffect(() => {
     port.setGateRef(gateRef.current);
   }, [gateRef]);
   return (
     <div className="Port" id={port.id}>
-      {port.isIn && (
-        <div
-          className="gate"
-          id={port.gateId}
-          onClick={onPortClick}
-          ref={gateRef}
-        />
-      )}
+      {port.isIn && gate}
       <div className="label">{port.label}</div>
-      {port.isOut && (
-        <div
-          className="gate"
-          id={port.gateId}
-          onClick={onPortClick}
-          ref={gateRef}
-        />
-      )}
+      {port.isOut && gate}
     </div>
   );
 };
